f26ad48ddfec88a49ffafb44ca05aa9b53a6da33
Scott Pham <scott.pham85@gmail.com>
2017-01-09 18:41:50 -0800

    changed sensor to use apsched instead of just sleep

--- sensor/management/commands/runSensor.py
+++ sensor/management/commands/runSensor.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 from django.utils import timezone
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 from sensor.models import Reading
 
@@ -21,14 +22,9 @@ class Command(BaseCommand):
         # date formatter
         formatter = "%Y-%m-%d %H:%M"
 
-        # states list
-        states = []
-
-
         def recordChange(value):
             # record time
             now = datetime.datetime.now().strftime(formatter)
-            #
             # if value == 1:
             #     print "There was movement at %s" % (now)
             # else:
@@ -39,26 +35,32 @@ class Command(BaseCommand):
 
             r.save()
 
-        # run this continuously
-        while True:
-            # wait a second
-            time.sleep(1)
+        def watch():
+            # states list
+            states = []
 
-            # take reading
-            current_state = GPIO.input(sensor)
+            while len(states) < 60:
+                # take reading
+                current_state = GPIO.input(sensor)
+                # write state to list of states
+                states.append(current_state)
+                # log state to the console
+                print current_state
+                sleep(1)
 
-            # write state to list of states
-            states.append(current_state)
+            # 60 seconds later...
+            # if there is 1 or more instance of '1'
+            if states.count(1) > 0:
+                recordChange(1)
+            else:
+                recordChange(0)
 
-            # log state to the console
-            # print current_state
+            #empty list
+            states = []
 
-            # after 60 seconds, log value to csv
-            if len(states) >= 60:
-                if states.count(1) >= 1:
-                    recordChange(1)
-                else:
-                    recordChange(0)
+        sched = BlockingScheduler()
 
-                #empty list
-                states = []
+        # Schedules job_function to be run once each minute
+        sched.add_job(watch, 'cron', minute='0-59')
+
+        sched.start()
