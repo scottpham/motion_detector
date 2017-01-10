c8597c7bbff73bd513a7539c55a94d572a207188
Scott Pham <scott.pham85@gmail.com>
2017-01-09 20:16:31 -0800

    use local d3

--- sensor/views.py
+++ sensor/views.py
@@ -7,18 +7,18 @@ import json
 from datetime import datetime
 from datetime import timedelta
 
-# Create your views here.
-def index(request):
-    readings = Reading.objects.all().order_by('-date_time')[:2000]
-
-    template = loader.get_template('sensor/index.html')
-
-    data = serializers.serialize('json', readings)
-
-    context = { 'readings': readings, 'data': data}
-
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'sensor/index.html', context)
+# # Create your views here.
+# def index(request):
+#     readings = Reading.objects.all().order_by('-date_time')[:2000]
+#
+#     template = loader.get_template('sensor/index.html')
+#
+#     data = serializers.serialize('json', readings)
+#
+#     context = { 'readings': readings, 'data': data}
+#
+#     # return HttpResponse(template.render(context, request))
+#     return render(request, 'sensor/index.html', context)
 
 def occupied(request):
     start_time = datetime.now()
@@ -43,7 +43,7 @@ def today(request):
     # get readings, filter for today only
     readings = Reading.objects.filter(date_time__day=today).order_by('-date_time')
 
-    template = loader.get_template('sensor/index.html')
+    # template = loader.get_template('sensor/index.html')
 
     data = serializers.serialize('json', readings)
 
